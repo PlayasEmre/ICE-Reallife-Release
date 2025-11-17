0829de95f6334683fb96c6ff935e86b13b2e4ef6
PlayasEmre <emresarnitz@gmail.com>
2025-11-17 22:50:13 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,7 @@
 --||   Version: 5.0                                   ||
 --\\                                                  //
 
-print("dasdasdsadasdsadasdsadsa")
+print("eeeeeeeeeeeeeeeeeeeeeeeeeeee")
 
 function mainTimer ()
 	local curtime = getRealTime()
